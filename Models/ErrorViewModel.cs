118816558a79d1a9dd13a1832338cbdf4092dadf
leydiMontufar <leydimontufar16@gmail.com>
2022-05-03 00:23:23 -0500

    updated

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace App_TrabajoFinal_USMP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
