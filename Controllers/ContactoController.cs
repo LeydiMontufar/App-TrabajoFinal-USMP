77d7d85bb8652d7174e0cd575404e641f968d659
leydiMontufar <leydimontufar16@gmail.com>
2022-05-03 16:08:32 -0500

    Contactos copleto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -4,25 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using App_TrabajoFinal_USMP.Models;
+using App_TrabajoFinal_USMP.Data;
+
+
 
 namespace App_TrabajoFinal_USMP.Controllers
-{   
-    public class ContactoController : Controller
+{
+    public class ContactoController: Controller
     {
         private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public ContactoController(ILogger<ContactoController> logger)
+        public ContactoController(ApplicationDbContext context,
+            ILogger<ContactoController> logger)
         {
+            _context = context;
             _logger = logger;
-        } 
+        }
+
         public IActionResult Index()
         {
+            
             return View();
         }
-        public IActionResult Create(Contacto obContacto)
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
         {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "Se registro el contacto";
             return View("Index");
         }
-         
+
+
     }
 }
